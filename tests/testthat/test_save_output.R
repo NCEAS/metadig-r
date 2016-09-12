2f3fbea9304f320bdccadd458678def49aa86371
Bryce Mecum <petridish@gmail.com>
2016-09-12 15:37:12 -0800

    Remove fixmes I fixed

--- tests/testthat/test_save_output.R
+++ tests/testthat/test_save_output.R
@@ -6,8 +6,8 @@ test_that("a text output can be saved", {
   save_output("test")
 
   expect_is(mdq_result, "list")
-  expect_length(mdq_result[["output"]], 1) # TODO: FIX THIS
-  expect_equal(mdq_result[["output"]][[1]][["value"]], "test") # TODO: FIX THIS
+  expect_length(mdq_result[["output"]], 1)
+  expect_equal(mdq_result[["output"]][[1]][["value"]], "test")
 })
 
 test_that("an output type that isn't supported cannot be saved", {
