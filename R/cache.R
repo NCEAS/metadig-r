cf2afeb6acf832fdd486f814e2ca3eb9ff87a2b9
Bryce Mecum <petridish@gmail.com>
2016-09-16 17:13:07 -0800

    Bring cache routine into package

---
+++ R/cache.R
@@ -0,0 +1,31 @@
+#' Get the file path for an object specified by its URL. If the content at the
+#' given URL has not been cached previously, the bytes are retrieved and cached.
+#'
+#' @param url (character) Any URL
+#'
+#' @return (character) The path to the file
+#' @export
+#'
+#' @examples
+mdq_get <- function(url) {
+  temp_dir = Sys.getenv("MDQE_CACHE_DIR", "~/tmp")
+  if (temp_dir == "") stop("MDQE_CACHE_DIR was not set.")
+  if (!file.exists(temp_dir))
+    stop(paste0("MDQE_CACHE_DIR was set to a path that does not exist: ",
+                temp_dir))
+
+  cache_dir = file.path(temp_dir, "mdq-cache")
+  if (!file.exists(cache_dir)) dir.create(cache_dir)
+  stopifnot(file.exists(cache_dir))
+
+  key = digest(url, algo = "md5", ascii = TRUE, serialize = FALSE)
+  file_path = file.path(cache_dir, key)
+
+  if (!file.exists(file_path)) {
+    request <- GET(url)
+    writeLines(content(request, as = "text"), con = file_path)
+  }
+  stopifnot(file.exists(file_path))
+
+  file_path
+}
