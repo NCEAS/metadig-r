145de152e616e7b05f1db225232b0090aa477e17
Jeanette Clark <jclark@nceas.ucsb.edu>
2023-01-06 10:57:25 -0700

    update tests and documentation for sysmeta arg

--- tests/testthat/test_run_checks.R
+++ tests/testthat/test_run_checks.R
@@ -3,26 +3,49 @@ context("check execution tests")
 test_that("runCheck() works", {
   checkFile <- system.file("extdata/dataset_title_length-check.xml", package = "metadig")
   metadataFile <- system.file("extdata/example_EML.xml", package = "metadig")
+  sysmetaXML <- system.file("extdata/example_sysmeta.xml", package = "metadig")
 
-  results <- runCheck(checkFile, metadataFile)
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
   result <- results[[1]]
   # The title for the test EML file is too short, so this check should fail.
   expect_match(result$status, "FAILURE")
   expect_match(result$output[[1]]$value, ".*minimum required word count")
 
   checkFile <- system.file("extdata/entity_attributes_sufficient_check.xml", package = "metadig")
-  results <- runCheck(checkFile, metadataFile)
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
   result <- results[[1]]
   # Some attributes descriptions aren't enough words, so this test should fail.
   expect_match(result$status, "FAILURE")
 
   checkFile <- system.file("extdata/datatype_check.xml", package = "metadig")
-  results <- runCheck(checkFile, metadataFile)
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
   result <- results[[1]]
   expect_match(result$status, "SUCCESS")
 
   checkFile <- system.file("extdata/methods_present.xml", package = "metadig")
-  results <- runCheck(checkFile, metadataFile)
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
   result <- results[[1]]
   expect_match(result$status, "SUCCESS")
+})
+
+test_that("runCheck() handles sysmetaXML argument correctly", {
+
+  checkFile <- system.file("extdata/dataset_title_length-check.xml", package = "metadig")
+  metadataFile <- system.file("extdata/example_EML.xml", package = "metadig")
+  # case 1: argument is a file that can be found
+  sysmetaXML <- system.file("extdata/example_sysmeta.xml", package = "metadig")
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
+  result <- results[[1]]
+  expect_match(result$status, "FAILURE")
+
+  # case2: argument is a string literal of sysmeta
+  sysmetaXML <- readChar(sysmetaXML, file.info(sysmetaXML)$size)
+  results <- runCheck(checkFile, metadataFile, sysmetaXML)
+  result <- results[[1]]
+  expect_match(result$status, "FAILURE")
+
+  # case3: argument is a file that doesn't exist
+  sysmetaXML <- "notafile.xml"
+  expect_warning(runCheck(checkFile, metadataFile, sysmetaXML))
+
 })
--- tests/testthat/test_run_suite.R
+++ tests/testthat/test_run_suite.R
@@ -4,15 +4,16 @@ test_that("runSuite() works", {
   metadataXML <- system.file("extdata/example_EML.xml", package = "metadig")
   suiteXML <- system.file("extdata/example_suite.xml", package = "metadig")
   dirXML <- system.file("extdata", package = "metadig")
+  sysmetaXML <- system.file("extdata/example_sysmeta.xml", package = "metadig")
 
-  expect_error(runSuite(7, 7, 7))
-  expect_error(runSuite(suiteXML, 7, 7))
-  expect_error(runSuite(suiteXML, dirXML, 7))
+  expect_error(runSuite(7, 7, 7, 7))
+  expect_error(runSuite(suiteXML, 7, 7, 7))
+  expect_error(runSuite(suiteXML, dirXML, 7, 7))
   expect_error(runSuite(c(suiteXML, dirXML), dirXML, metadataXML))
   expect_error(runSuite(suiteXML, c(suiteXML, dirXML), metadataXML))
   expect_error(runSuite(suiteXML, dirXML, c(suiteXML, dirXML)))
 
-  results <- runSuite(suiteXML, dirXML, metadataXML)
+  results <- runSuite(suiteXML, dirXML, metadataXML, sysmetaXML)
 
   expect_match(results[[1]]$value$status, "FAILURE")
   expect_match(results[[2]]$value$status, "SUCCESS")
