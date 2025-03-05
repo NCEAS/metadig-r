82a1b091d99cdc2fb6739a001442def8319bb7c9
Lindsay RC Platt <lplatt@cuahsi.org>
2025-03-05 14:01:06 -0600

    this should have been deleted with https://github.com/NCEAS/metadig-r/pull/9/commits/fae1c7ca4efe30f92b6cf25466b82a8e0f65e796

--- R/runCheck.R
+++ R/runCheck.R
@@ -319,44 +319,6 @@ ns_strip <- function(x, nsPrefix) {
   invisible(x)
 }
 
-
-#' @example
-#' json_apply_jpath(
-#'  json_context = list(
-#'    `@context` = "https://schema.org",
-#'    url = "https://doi.org/10.4211/hs.e99ee3f096964b31add85947894d812c",
-#'    name = "Virginia Forest - DTW, Water Temperature, Specific conductance - Jun 2021-Jul 2024"
-#'  ), jpath_text = "$name;!is.null")
-#'
-
-json_apply_jpath <- function(json_context, jpath_text) {
-
-  # First separate out if there are multiple commands to use. The assumption
-  # is that they are separated by `;`.
-  jpath_actions <- unlist(strsplit(jpath_text, ';'))
-
-  # For the first action, value is the full `json_context`
-  value <- json_context
-  for(act in jpath_actions) {
-
-    if(grepl('^\\$', act)) {
-      # If the action starts with `$`, it means we are simply subsetting
-      # So, add the data we are subsetting to the front of the action
-      act <- paste0("value", act)
-    } else {
-      # Otherwise, it is a function that our data is passed to
-      act <- paste0(act, "(value)")
-    }
-
-    # Now, apply the action and write over `value` so that it is ready
-    # for the next loop iteration.
-    value <- eval(parse(text=act))
-
-  }
-
-  return(value)
-}
-
 # Simple helper function to extract the value of `@context` from
 # JSON when it is already loaded into the environment as one giant
 # string. Could more easily find the key-value for context by parsing
